f99854bd2225a75a2a98c505a202dd3f8f6e0c60
Dmitry <axiumone@gmail.com>
2020-09-02 18:20:36 -0400

    fixed cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,17 @@
 class ApplicationController < ActionController::API
     before_action :authorized
- 
-    def encode_token(payload)
-      # should store secret in env variable
-      JWT.encode(payload, 'my_s3cr3t')
+  
+    def encode_token(payload) #{ user_id: 2 }
+      JWT.encode(payload, 'my_s3cr3t') #issue a token, store payload in token
     end
-   
+  
     def auth_header
-      # { Authorization: 'Bearer <token>' }
-      request.headers['Authorization']
+      request.headers['Authorization'] # Bearer <token>
     end
-   
+  
     def decoded_token
-      if auth_header
-        token = auth_header.split(' ')[1]
-        # header: { 'Authorization': 'Bearer <token>' }
+      if auth_header()
+        token = auth_header.split(' ')[1] #[Bearer, <token>]
         begin
           JWT.decode(token, 'my_s3cr3t', true, algorithm: 'HS256')
         rescue JWT::DecodeError
@@ -22,19 +19,21 @@ class ApplicationController < ActionController::API
         end
       end
     end
-   
+  
     def current_user
-      if decoded_token
-        user_id = decoded_token[0]['user_id']
+      if decoded_token()
+        user_id = decoded_token[0]['user_id'] 
         @user = User.find_by(id: user_id)
+      else
+        nil
       end
     end
-   
+  
     def logged_in?
       !!current_user
     end
-   
+  
     def authorized
       render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
     end
-end
+  end
